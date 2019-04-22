daa655aacecd06476a965d15f95d5dc93ade9989
Joe Barnes and Michael Larragueta <michael@>
2019-04-22 13:27:43 -0700

    add functionality

---
+++ TriangleTracker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TriangleTracker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length for side 1");
+            int side1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter length for side 2");
+            int side2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter length for side 3");
+            int side3 = int.Parse(Console.ReadLine());
+
+            Triangle shape = new Triangle(side1, side2, side3);
+            int holdNum = shape.IsTriangle(side1, side2, side3);
+            if (holdNum == 1)
+            {
+                Console.WriteLine("It is an Equilateral Triangle");
+            }
+            else if (holdNum == 2)
+            {
+                Console.WriteLine("It is an Isosceles Triangle");
+            }
+            else if (holdNum == 3)
+            {
+                Console.WriteLine("It is a Scalene Triangle");
+            }
+            else if (holdNum == 0)
+            {
+                Console.WriteLine("NOT A TRIANLGE");
+            }
+            else Console.WriteLine("HOW DID YOU GET HERE");
+        }
+    }
+}
---
+++ TriangleTracker/Triangle/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace TriangleTracker
+{
+     class Triangle
+    {
+        private int Side1;
+        private int Side2;
+        private int Side3;
+
+
+        public Triangle(int side1, int side2, int side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+        //is a triangle
+        //side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1 && side1 !== 0 && side2 !== 0 && side3 !== 0
+        //side1 === side2 && side1 === side3 && side2 === side3 equilateral
+        //side1 === side2 || side1 === side3 || side2 === side3 isosceles
+        //side1 !== side2 && side1 !== side3 && side2 !== side3 scalene
+        public int IsTriangle(int side1, int side2, int side3)
+        {
+            if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1 && side1 != 0 && side2 != 0 && side3 != 0)
+            {
+                if (side1 == side2 && side1 == side3 && side2 == side3)
+                {
+                    return 1;
+                }
+                else if (side1 == side2 || side1 == side3 || side2 == side3)
+                {
+                    return 2;
+                }
+                else if (side1 != side2 && side1 != side3 && side2 != side3)
+                {
+                    return 3;
+                }
+                else return 0;
+
+            }
+            else return 0;
+        }
+    }
+}
